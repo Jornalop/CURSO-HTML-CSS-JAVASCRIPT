57361bca83a80e282531df31fcd466fda8eb215a
Jornalop <jornalop99@gmail.com>
2024-07-24 09:58:29 -0300

    Avances

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let botonPlay = document.getElementById ("botonPlay")
+let botonPausa = document.getElementById ("botonPausa")
+let video = document.querySelector('video');
+
+
+botonPlay.addEventListener('click', () =>{
+    video.play();
+});
+
+botonPausa.addEventListener('click', () =>{
+    video.pause();
+});
